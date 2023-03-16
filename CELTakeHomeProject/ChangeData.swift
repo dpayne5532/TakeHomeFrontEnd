cdaab4496e7fcccde22f4f12402acef90c7c03ce
Dan Payne <dan@danpayne.info>
2023-03-16 11:51:25 -0600

    add currency assets

--- CELTakeHomeProject/ChangeData.swift
+++ CELTakeHomeProject/ChangeData.swift
@@ -9,17 +9,18 @@ import Foundation
 
 
 struct ChangeData: Codable {
-
-     var hundreds: Int?
-     var fifties: Int?
-     var twenties: Int?
-     var tens: Int?
-     var fives: Int?
-     var twos: Int?
-     var ones: Int?
-     var quarters: Int?
-     var dimes: Int?
-     var nickels: Int?
-     var pennies: Int?
+  
+  var hundreds: Int?
+  var fifties: Int?
+  var twenties: Int?
+  var tens: Int?
+  var fives: Int?
+  var twos: Int?
+  var ones: Int?
+  var quarters: Int?
+  var dimes: Int?
+  var nickels: Int?
+  var pennies: Int?
+  var gitHubRepo: String
   
 }
--- CELTakeHomeProject/ContentView.swift
+++ CELTakeHomeProject/ContentView.swift
@@ -9,17 +9,17 @@ import SwiftUI
 
 
 class Change: ObservableObject {
-  @Published var hundreds: Int?
-  @Published var fifties: Int?
-  @Published var twenties: Int?
-  @Published var tens: Int?
-  @Published var fives: Int?
-  @Published var twos: Int?
-  @Published var ones: Int?
-  @Published var quarters: Int?
-  @Published var dimes: Int?
-  @Published var nickels: Int?
-  @Published var pennies: Int?
+  @Published var hundreds: Int = 1
+  @Published var fifties: Int = 1
+  @Published var twenties: Int = 4
+  @Published var tens: Int = 1
+  @Published var fives: Int = 1
+  @Published var twos: Int = 0
+  @Published var ones: Int = 2
+  @Published var quarters: Int = 0
+  @Published var dimes: Int = 0
+  @Published var nickels: Int = 0
+  @Published var pennies: Int = 0
 }
 
 
@@ -29,24 +29,45 @@ class Change: ObservableObject {
 
 struct ContentView: View {
   
-  let changeURL = "http://localhost:3010/"
+  let changeURL = "https://frosty-glade-1979.fly.dev/"
   
-  @State private var amount:Float = 0.00
+  @State private var amount:Float = 198.15
   @ObservedObject var changeData = Change()
   
   private let numberFormatter: NumberFormatter
-      
-      init() {
-        numberFormatter = NumberFormatter()
-        numberFormatter.numberStyle = .currency
-        numberFormatter.maximumFractionDigits = 2
-      }
+  
+  init() {
+    numberFormatter = NumberFormatter()
+    numberFormatter.numberStyle = .currency
+    numberFormatter.maximumFractionDigits = 2
+  }
   
   var body: some View {
     VStack {
+      VStack {
+        HStack {
+          Image("hundreds\(changeData.hundreds)")
+            .padding(.leading)
+          Image("fifties\(changeData.fifties)")
+            .padding()
+          Image("twentie\(changeData.twenties)")
+            .padding(.trailing)
+        }
+        HStack {
+          Image("tens\(changeData.tens)")
+            .padding(.leading)
+          Image("fives\(changeData.fives)")
+            .padding()
+          Image("one\(changeData.ones)")
+            .padding(.trailing)
+          
+          
+        }
+      }
       
+      Spacer()
       HStack {
-      
+        
         TextField("amount", value: $amount, formatter: numberFormatter)
           .font(.title)
           .bold()
@@ -54,14 +75,25 @@ struct ContentView: View {
         
       }
       
-      
+      Button(action: doStuff) {
+        Text("Change!")
+        
+          .frame(width: 150, height: 80)
+          .background(.blue)
+          .foregroundColor(.white)
+          .bold()
+          .font(.title2)
+          .clipShape(Capsule())
+        
+      }
     }
     .padding()
   }
   
   
-  func fetchQuote(amount: Int) {
-    let urlString = "\(changeURL)"
+  func fetchQuote(amount: Float) {
+    let urlString = "\(changeURL)\(amount)"
+    print("urlString: ", urlString)
     performRequest(urlString: urlString)
   }
   
@@ -89,7 +121,19 @@ struct ContentView: View {
       let decodedData = try decoder.decode(ChangeData.self, from: changeData)
       
       DispatchQueue.main.async {
-        self.changeData.dimes = decodedData.dimes
+        self.changeData.hundreds = decodedData.hundreds ?? 0
+        self.changeData.fifties = decodedData.fifties ?? 0
+        self.changeData.twenties = decodedData.twenties ?? 0
+        self.changeData.tens = decodedData.tens ?? 0
+        self.changeData.fives = decodedData.fives ?? 0
+        self.changeData.ones = decodedData.ones ?? 0
+        
+        
+        self.changeData.dimes = decodedData.dimes ?? 0
+        print("hundreds: ", self.changeData.hundreds)
+        print("Fifties: ", self.changeData.fifties)
+        print(self.changeData.hundreds)
+        
       }
     } catch {
       print(error.localizedDescription)
@@ -97,8 +141,9 @@ struct ContentView: View {
   }
   
   func doStuff() {
+    
     DispatchQueue.global(qos: .userInitiated).async {
-      // fetchQuote(amount: amount)
+       fetchQuote(amount: amount)
     }
   }
   
